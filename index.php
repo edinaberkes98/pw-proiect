5ab854bdae9aa9b9611e62da2c66c990043b29f6
unknown <edina.berkes98@gmail.com>
2020-05-09 17:06:02 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <title>Plot the plot</title>
+
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center" style="  background-image: url(background.jpg);">
+
+    <div class="container d-flex h-100 p-3 mx-auto flex-column" >
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Plot the Plot</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="index.html">Home</a>
+            <a class="nav-link" href="plot.php">Plot</a>
+    	      <a class="nav-link" href="history.php">History</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Just Plot</h1>
+        <p class="lead">Plot graphical representations of your data using iteration plotting.</p>
+        <p class="lead">
+          <a href="plot.php" class="btn btn-lg btn-secondary">Start Plot</a>
+        </p>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+         
+        </div>
+      </footer>
+    </div>
+
+  </body>
+</html>
---
+++ submit-plot.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    
+    <?php
+      echo("form");
+      echo "<hr>"
+      ?>
+
+<form action="generate-plot.php" method="post">
+        Name: <input type="text" name="name" required>
+        <br>
+        Age: <input type="number" name="age" required>
+        <br>
+        <input type="submit">
+</form>
+
+  </body>
+</html>
