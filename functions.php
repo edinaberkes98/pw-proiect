c762edfed3b9faa7dd1ed841bc4a15a517ec1313
unknown <edina.berkes98@gmail.com>
2020-05-13 12:24:00 +0300

    add code

--- functions.php
+++ functions.php
@@ -8,17 +8,17 @@ function generatePlot($plot, $data, $format = 'png') {
     ->setHeight($data['height']);
 
     $plot->setTitle(0, $data['dataset_name']);
-    $dataset = explode(",", $data['dataset']); // 1, 2, 3, 4
+    $dataset = explode(",", $data['dataset']); // 1, 2, 3, 4 to [1, 2, 3, 4]
 
     foreach ($dataset as $key => $datapoint) {
         $plot->push($key, $datapoint);
     }
 
     $plot->setTitle(1, $data['dataset_name2']);
-    $dataset = explode(",", $data['dataset2']); // 1, 2, 3, 4
+    $dataset = explode(",", $data['dataset2']); // 1, 2, 3, 4 to [1, 2, 3, 4]
 
     foreach ($dataset as $key => $datapoint) {
-        $plot->push($key, $datapoint);
+        $plot->push($key, $datapoint, 1);
     }
 
     switch($format) {
